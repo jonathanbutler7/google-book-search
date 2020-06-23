6bd4278d73a81c4ed792b1f7fac9c10c118d5804
jonathanbutler7 <jonathanbutler7@gmail.com>
2020-06-22 19:26:41 -0700

    Updates

---
+++ precache-manifest.8a2887d16fae9208e1609841eefd2f1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "427ed1cf1e746daaa1d71a462cc1dfa8",
+    "url": "/google-book-search/index.html"
+  },
+  {
+    "revision": "17ac6f75c6dc3ba57f02",
+    "url": "/google-book-search/static/css/main.f30f9307.chunk.css"
+  },
+  {
+    "revision": "08acab71c54ec43b77d2",
+    "url": "/google-book-search/static/js/2.6709c213.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/google-book-search/static/js/2.6709c213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17ac6f75c6dc3ba57f02",
+    "url": "/google-book-search/static/js/main.f8784367.chunk.js"
+  },
+  {
+    "revision": "de79a0128f5173df6f00",
+    "url": "/google-book-search/static/js/runtime-main.4ba68510.js"
+  }
+]);
